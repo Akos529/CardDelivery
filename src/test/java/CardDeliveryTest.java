68ed8fe69127a24cc488077f5b950f7a6610099c
Taisia Ivanovskaya <taya.naumova@yahoo.com>
2023-03-05 22:35:38 +0300

    Task completed

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,6 +1,26 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
-
-
-
+    @BeforeEach
+    void openBrowser() {
+        open("http://localhost:9999");
+    }
+    @Test
+    void shouldSubmitRequest(){
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
+        form.$("[data-test-id=date] input").setValue("10.03.2023");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79210000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(withText("Успешно!")).waitUntil(exist, 15000);
+    }
 }
