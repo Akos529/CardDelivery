b2b0e04637781eddeab6d65181b8edd396518735
Taisia Ivanovskaya <taya.naumova@yahoo.com>
2023-03-10 11:30:01 +0300

    Test corrected

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,6 +1,12 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.withText;
@@ -8,6 +14,10 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
     @BeforeEach
     void openBrowser() {
         open("http://localhost:9999");
@@ -16,11 +26,15 @@ public class CardDeliveryTest {
     void shouldSubmitRequest(){
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
-        form.$("[data-test-id=date] input").setValue("10.03.2023");
+        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(generateDate(5));
         form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+79210000000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(withText("Успешно!")).waitUntil(exist, 15000);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(5)), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
